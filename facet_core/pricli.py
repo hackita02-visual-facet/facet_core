eeb4b156c34c90c9c7340e238ae332dfb3477efe
Amit Ziv-Kenet <amit.zivkenet@gmail.com>
2015-11-11 12:27:33 +0200

    added option to get facet object from cli

--- facet_core/pricli.py
+++ facet_core/pricli.py
@@ -1,3 +1,5 @@
+import json
+
 import click
 import requests
 
@@ -10,18 +12,27 @@ def cli():
 @cli.command()
 @click.argument('query')
 @click.option('--query-param','-qa','query_params',nargs=2,multiple=True)
-@click.option('--json/--xml',default=False)
+@click.option('--json','-j','use_json',is_flag=True)
+@click.option('--facets','-f',is_flag=True)
 @click.option('--post','-p','post_to')
 @click.option('--post-header','-ph','post_headers',nargs=2,multiple=True)
-def brief(query,query_params,json,post_to,post_headers):
-	
-	res = primo.brief_query(query,json,**dict(query_params))
+def brief(query,query_params,use_json,facets,post_to,post_headers):
+			
+	qps = dict(query_params)
+	if use_json:
+		qps['json'] = True
+
+	res = primo.brief_query(query,**qps)
+
+	if facets:
+		res = json.dumps(primo.parse_facets(res))
+
 	click.echo(res)
 
 	if post_to:
 
 		headers = {
-			"Content-Type"	:	"application/json" if json else "text/xml"
+			"Content-Type"	:	"application/json" if use_json else "text/xml"
 		}
 
 		headers.update(dict(post_headers))
