ca6d954e60489e9f0089c47733c4e341de80ab97
Amit Ziv-Kenet <amit.zivkenet@gmail.com>
2015-11-11 07:45:11 +0200

    added function for parsing json facets

--- facet_core/primo.py
+++ facet_core/primo.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from lxml import etree
 
 PRIMO_URL = "http://primo.nli.org.il/PrimoWebServices/xservice/search"
 
@@ -26,3 +28,31 @@ def brief_query(query,json = False,**query_params):
 		return res.content
 
 	res.raise_for_status()
+
+
+def _facets_from_json(jsn_res):
+
+	_s = lambda x: "sear:%s"%x
+
+	d = json.loads(jsn_res)
+
+	try:
+		facet_list = d[_s("SEGMENTS")][_s("JAGROOT")]\
+		[_s("RESULT")][_s("FACETLIST")][_s("FACET")]
+	except KeyError:
+		return []
+
+	facets = {}
+	for facet_d in facet_list:
+		fd = {
+			"count"	:	int(facet_d['@COUNT']),
+			"values"	:	{}
+		}
+
+		for kv in facet_d[_s("FACET_VALUES")]:
+			fd["values"][kv["@KEY"]] = int(kv["@VALUE"])
+
+		facets[facet_d['@NAME']] = fd
+
+	return facets
+
