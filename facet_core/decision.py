baa6aeb8435b8c7070190221a32b069879cb4946
Amit Ziv-Kenet <amit.zivkenet@gmail.com>
2015-11-12 20:35:43 +0200

    tabs to spaces

--- facet_core/decision.py
+++ facet_core/decision.py
@@ -1,23 +1,22 @@
 from operator import itemgetter
 
+
 def _top_facet_ratio(facet):
-	ratios = list(facet.values())
+    ratios = list(facet.values())
 
-	if len(ratios) < 2:
-		return 0
+    if len(ratios) < 2:
+        return 0
 
-	ratios.sort(reverse=True)
+    ratios.sort(reverse=True)
 
-	return float(ratios[0])/ratios[1]
-
-def max_split(facets,top=None):
-
-	top_ratios = [(k,_top_facet_ratio(v)) for k,v in facets.items()	]
-	top_ratios.sort(key=itemgetter(1),reverse=True)
-	
-	if top is not None and len(top_ratios) >= top:
-		return top_ratios[:top]
-	else:
-		return top_ratios
+    return float(ratios[0]) / ratios[1]
 
 
+def max_split(facets, top=None):
+    top_ratios = [(k, _top_facet_ratio(v)) for k, v in facets.items()]
+    top_ratios.sort(key=itemgetter(1), reverse=True)
+
+    if top is not None and len(top_ratios) >= top:
+        return top_ratios[:top]
+    else:
+        return top_ratios
