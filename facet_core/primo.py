6c695efaff34e09e443ca67ca13c30ae1de42ea9
Amit Ziv-Kenet <amit.zivkenet@gmail.com>
2015-11-22 10:38:44 +0200

    added possibility to return total number of results from facet query

--- facet_core/primo.py
+++ facet_core/primo.py
@@ -28,7 +28,8 @@ def brief_query(query, **query_params):
     res.raise_for_status()
 
 
-def _facets_from_json(jsn_res):
+def _facets_from_json(jsn_res, query_total):
+    print("Trying to parse using JSON parser")
     _s = lambda x: "sear:%s" % x
 
     d = json.loads(jsn_res)
@@ -47,10 +48,24 @@ def _facets_from_json(jsn_res):
 
         facets[facet_d['@NAME']] = fd
 
+    if query_total:
+        try:
+            total = d[_s("SEGMENTS")][_s("JAGROOT")] \
+            [_s("RESULT")][_s("DOCSET")]["@TOTALHITS"]
+
+        except KeyError:
+            total = -1
+
+        facets = {
+                "total" :   int(total),
+                "facets"    :   facets
+            }
+
     return facets
 
 
-def _facets_from_xml(xml_res):
+def _facets_from_xml(xml_res, query_total):
+    print("Trying to parse using XML parser")
     _s = lambda x: "{http://www.exlibrisgroup.com/xsd/jaguar/search}%s" % x
 
     x = etree.fromstring(xml_res)
@@ -68,22 +83,37 @@ def _facets_from_xml(xml_res):
 
         facets[facet_el.get("NAME")] = fd
 
+    if query_total:
+        total = -1
+        docset = x.find(".//%s" % _s("DOCSET"))
+
+        if docset is not None:
+            total = docset.get("TOTALHITS")
+
+            if total is None:
+                total = int(total)
+
+        facets = {
+            "total" :   total,
+            "facets"    :   facets
+        }
+
     return facets
 
 
-def parse_facets(res):
+def parse_facets(res, query_total):
     try:
-        facets = _facets_from_json(res)
+        facets = _facets_from_json(res, query_total)
     except ValueError:
         try:
-            facets = _facets_from_xml(res)
+            facets = _facets_from_xml(res, query_total)
         except etree.XMLSyntaxError:
             raise ValueError("Invalid primo response")
 
     return facets
 
 
-def facet_query(query, **query_params):
+def facet_query(query, query_total = True, **query_params):
     res = brief_query(query, **query_params)
 
-    return parse_facets(res)
+    return parse_facets(res, query_total)
